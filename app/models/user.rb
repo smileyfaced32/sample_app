b50d2689372b9ebc1ff72df5b3176a65da6d2820
smileyfaced32 <fiore.dennis@gmail.com>
2010-10-23 21:18:10 -0400

    User Model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,12 @@
 
 class User < ActiveRecord::Base
   attr_accessible :name, :email
+
+  email_regex = /^([\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+\.)*[\w\!\#$\%\&\'\*\+\-\/\=\?\^\`{\|\}\~]+@((((([a-z0-9]{1}[a-z0-9\-]{0,62}[a-z0-9]{1})|[a-z])\.)+[a-z]{2,6})|(\d{1,3}\.){3}\d{1,3}(\:\d{1,5})?)$/i
+  
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
+  validates :email, :presence   => true,
+            :format     => { :with => email_regex },
+            :uniqueness => { :case_sensitive => false }
 end
