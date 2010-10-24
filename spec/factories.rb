1acbba4f2dcce614d22bb915230d3c20b2d61f7a
smileyfaced32 <fiore.dennis@gmail.com>
2010-10-24 12:40:04 -0400

    Added factories and test for show

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Dennis Fiore"
+  user.email                 "fiore.dennis@gmail.com"
+  user.password              "dfiore30"
+  user.password_confirmation "dfiore30"
+end
