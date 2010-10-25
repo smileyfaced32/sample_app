88d14e0d6f805ba3341c197284bbbc52ba45c31c
smileyfaced32 <fiore.dennis@gmail.com>
2010-10-25 17:44:34 -0400

    Working Index Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
+  
+  def index
+    @users = User.all
+    @title = "All users"
+  end
   
   def show
     @user = User.find(params[:id])
@@ -24,6 +29,7 @@ class UsersController < ApplicationController
       end
     end
     def edit
+      @user = User.find(params[:id])
         @title = "Edit user"
       end
 
